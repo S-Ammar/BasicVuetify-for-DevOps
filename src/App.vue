240b440612a0b86455a78cd82a1e087db061bdf1
S-Ammar <ammar.seifeddine@gmail.com>
2020-07-16 21:47:17 +0200

    Update App.vue
    change

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Hello DevOps"/>
+    <HelloWorld msg="Hello DevOps !!"/>
   </div>
 </template>
 
